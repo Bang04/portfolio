8efa8bc8b3132e22d816f4fb5b9fe04c808d4efd
Bang04 <eunkyeong_b@naver.com>
2024-04-14 11:20:25 +0900

    style : 미디어 쿼리 및 스타일 수정

--- src/content/About.js
+++ src/content/About.js
@@ -77,84 +77,90 @@ const About = forwardRef(function About(props, ref) {
     //     caption: 'main'
     //   },
     // ];
+    const nrems_web = "http://www.nrems.co.kr/login.php";
+    const nrems_app = "https://play.google.com/store/apps/details?id=kr.co.nrems";
 
     return(
       <section className={classes.section} ref={ref}>
-          <div  className={classes.header}>
-            <p className={classes.title}>About</p>
-            <p className={classes.sub_title}>간략한 소개</p>
-          </div>
-          {/* <div className={classes.box_title}>소개</div> */}
-          <div className={classes.box}>
-              <div className={classes.box_content}>
-                <div className={classes.name}>방은경</div>
-                <div className={classes.box_row_2}>
-                  <div><img src={cellphone} className={classes.icon}/>010.4122.0403</div>
-                  <div><img src={email} className={classes.icon}/>eunkyeong_b@naver.com</div>
-                  <div><img src={github} className={classes.icon}/><a href="https://github.com/Bang04/Bang04" target="_blank">github 방문하기</a></div>
-                  <div><img src={velog} className={classes.icon}/><a href="https://velog.io/@bang04"  target="_blank">velog 방문하기</a> </div>
-                </div>
-               
-               <div className={classes.box_col_1}>
-                  <div><b>-</b> 정보처리기사 최종합격</div>
-                  <div><b>-</b> 에이콘 아카데미 프론트엔드 양성과정 수료</div>
-                  <div><b>-</b> 세종사이버대학교  졸업 </div>
-                </div>
+        <div  className={classes.header}>
+          <p className={classes.title}>About</p>
+          <p className={classes.sub_title}>간략한 소개</p>
+        </div>
+
+        {/* 기본정보 */}
+        <div className={classes.box}>
+            <div className={classes.box_content}>
+              <div className={classes_a.name}>방은경</div>
+
+              <div className={classes_a.box_row_2}>
+                <div><img src={cellphone} className={classes_a.icon}/>010.4122.0403</div>
+                <div><img src={email} className={classes_a.icon}/>eunkyeong_b@naver.com</div>
+                <div><img src={github} className={classes_a.icon}/><a href="https://github.com/Bang04/Bang04" target="_blank">github 방문하기</a></div>
+                <div><img src={velog} className={classes_a.icon}/><a href="https://velog.io/@bang04"  target="_blank">velog 방문하기</a> </div>
+              </div>
               
-             </div>
+              <div className={classes_a.box_col_1}>
+                <div><b>-</b> 정보처리기사 최종합격</div>
+                <div><b>-</b> 에이콘 아카데미 프론트엔드 양성과정 수료</div>
+                <div><b>-</b> 세종사이버대학교  졸업 </div>
+              </div>
+            </div>
+        </div>
+
+        {/* 스킬뱃지 */}
+        <div className={classes.box_title}>스킬</div>
+        <div className={classes.box}>
+          <div className={classes_a.group}>
+            <div className={classes_a.type}>Front-End:</div>
+            <div className={classes.badges}>
+              <div className={classes.badge}>html5</div>
+              <div className={classes.badge}>css3</div>
+              <div className={classes.badge}>javascript</div>
+              <div className={classes.badge}>jquery</div>
+              <div className={classes.badge}>bootstrap</div>
+            </div>
           </div>
 
-          <div className={classes.box_title}>스킬</div>
+          <div className={classes_a.group}>
+            <div className={classes_a.type}>Back-End:</div>
+            <div className={classes.badges}>
+              <div className={classes.badge}>php</div>
+            </div>
+          </div>
+
+          <div className={classes_a.group}>
+            <div className={classes_a.type}>Database: </div>
+            <div className={classes.badges}>
+              <div className={classes.badge}>mysql</div>
+            </div>
+          </div>
+
+          <div className={classes_a.group}>
+            <div className={classes_a.type}>Studying:</div>
+            <div className={classes.badges}>
+              <div className={classes.badge}>react</div>
+              <div className={classes.badge}>NextJS</div>
+              <div className={classes.badge}>typescript</div>
+              <div className={classes.badge}>git</div>
+            </div>
+          </div>
+
+          <div className={classes_a.group}>
+            <div className={classes_a.type}>Experience:</div>
+            <div className={classes.badges}>
+              <div className={classes.badge}>java</div>
+              <div className={classes.badge}>spring</div>
+              <div className={classes.badge}>oracle</div>
+            </div>
+          </div>
+        </div>
+
+        {/* 프로젝트 모음 */}
+        <div className={classes.box_title}>최근 프로젝트</div>
+        <div className={classes.row2}>
+          {/* 프로젝트1 */}
           <div className={classes.box}>
-              <div className={classes_a.group}>
-                <div className={classes_a.type}>Front-End:</div>
-                <div className={classes_a.badges}>
-                  <div className={classes_a.badge}>html5</div>
-                  <div className={classes_a.badge}>css3</div>
-                  <div className={classes_a.badge}>javascript</div>
-                  <div className={classes_a.badge}>jquery</div>
-                  <div className={classes_a.badge}>bootstrap</div>
-                </div>
-              </div>
-
-              <div className={classes_a.group}>
-                <div className={classes_a.type}>Back-End:</div>
-                <div className={classes_a.badges}>
-                  <div className={classes_a.badge}>php</div>
-                </div>
-              </div>
-
-              <div className={classes_a.group}>
-                <div className={classes_a.type}>Database: </div>
-                <div className={classes_a.badges}>
-                  <div className={classes_a.badge}>mysql</div>
-                </div>
-              </div>
-
-              <div className={classes_a.group}>
-                <div className={classes_a.type}>Studying:</div>
-                <div className={classes_a.badges}>
-                  <div className={classes_a.badge}>react</div>
-                  <div className={classes_a.badge}>NextJS</div>
-                  <div className={classes_a.badge}>typescript</div>
-                  <div className={classes_a.badge}>git</div>
-                </div>
-              </div>
-
-              <div className={classes_a.group}>
-                <div className={classes_a.type}>Experience:</div>
-                <div className={classes_a.badges}>
-                  <div className={classes_a.badge}>java</div>
-                  <div className={classes_a.badge}>spring</div>
-                  <div className={classes_a.badge}>oracle</div>
-                </div>
-              </div>
-          </div>
-
-          <div className={classes.box_title}>최근 프로젝트</div>
-          <div className={classes.row2}>
-            <div className={classes.box}>
-                <div className={classes_a.sub_title}>솔라리버 태양광 모니터링 Web& QR Scanner APP​​​​</div>
+              <div className={classes_a.line}>
                 <Slide>
                   {slideImages.map((slideImage, index)=> (
                       <div key={index}>
@@ -164,17 +170,36 @@ const About = forwardRef(function About(props, ref) {
                       </div>
                     ))} 
                 </Slide>
+              </div>
+              <div className={classes.box_content}>
+              <div className={classes.title}>솔라리버 태양광 모니터링 Web& QR Scanner APP​​​</div>
+              <div className={classes.desciption}>마크다운 파일을 활용한 gatsby 프레임워크 블로그</div>
+              <div className={classes.stack}>
+                <div className={classes.title}>기능</div>
+                <p><span>기간</span>2022-08-01 ~ 2023-04-01</p>
+                <p><span>역할</span>web, app 기획, 설계, 개발, 빌드, 배포 경험 (80%)</p>
+                <p><span>기술스택</span></p>
+                <div className={classes.badges}>
+                  <div className={classes.badge}>Html</div>
+                  <div className={classes.badge}>Css3</div>
+                  <div className={classes.badge}>PHP</div>
+                  <div className={classes.badge}>JavaScript</div>
+                  <div className={classes.badge}>Mysql</div>
+                  <div className={classes.badge}>Ajax</div>
+                  <div className={classes.badge}>Jquery</div>
+                  <div className={classes.badge}>Android</div>
+                </div>
 
-                <div className={classes_a.sub_content}>
-                  <div><span className={classes_a.sub_span}>기간</span>2022-08-01 ~ 2023-04-01</div>
-                  <div><span className={classes_a.sub_span}>담당 역할</span>web, app 기획, 설계, 개발, 빌드, 배포 경험 (80%)​</div>
-                  <div><span className={classes_a.sub_span}>기술 스택 </span>PHP , JavaScript , Mysql, Ajax, Jquery ,Android​</div>
-                  <div><span className={classes_a.sub_span}>업무성과</span>기획, 퍼블리싱, 구현, 배포, 현장에서 어플 사용</div>
-                </div> 
-            </div>  
-          
-            <div className={classes.box}>
-              <div className={classes_a.sub_title}>REMP 태양광 모니터링 Web& 하이브리드 APP​​​​​​​​</div>
+                <div className={classes.title}>업무성과</div>
+                  <p>기획, 퍼블리싱, 구현, 배포, 현장에서 어플 사용 ​</p>
+
+              
+                </div>
+              </div>
+          </div>  
+          {/* 프로젝트2 */}
+          <div className={classes.box}>
+              <div className={classes_a.line}>
               <Slide>
                 {slideImages2.map((slideImage, index)=> (
                     <div key={index}>
@@ -184,47 +209,43 @@ const About = forwardRef(function About(props, ref) {
                     </div>
                   ))} 
               </Slide>
+            </div>
 
-
-              <div className={classes.box}>
-                <div className={classes.title}>Gatsby blog</div>
-                <div className={classes.desciption}>마크다운 파일을 활용한 gatsby 프레임워크 블로그</div>
-                <div className={classes.stack}>
-                  <div className={classes.title}>기능</div>
-                  <p><span>기간</span>2022-03-01 ~ 2022-06-01</p>
-                  <p><span>역할</span>web, app 유지보수 및 배포 경험 (50%)</p>
-                  <p><span>기술스택</span> PHP , JavaScript , Mysql, Ajax, Jquery,Android</p>
-                  <div className={classes.title}>업무성과</div>
-                    <p>1. Firebase 클라우드 메시징 푸시 기능​ 개선 ​</p>
-                    <p>2. 토큰 생성 API 업데이트</p>
-                    <p>3. 레거시 코드 리팩토링(login, webview)</p> 
-
-                    <div className={classes.title}>업무성과</div>
-                    <p>-  http://www.nrems.co.kr/login.php</p>
-                    <p>-  https://play.google.com/store/apps/details?id=kr.co.nrems</p>
-                  </div>
+            <div className={classes.box_content}>
+              <div className={classes.title}>REMP 태양광 모니터링 Web& 하이브리드 APP</div>
+              <div className={classes.desciption}>마크다운 파일을 활용한 gatsby 프레임워크 블로그</div>
+              <div className={classes.stack}>
+                <p><span>기간</span>2022-03-01 ~ 2022-06-01</p>
+                <p><span>역할</span>web, app 유지보수 및 배포 경험 (50%)</p>
+                <p><span>기술스택</span></p>
+                <div className={classes.badges}>
+                  <div className={classes.badge}>Html</div>
+                  <div className={classes.badge}>Css3</div>
+                  <div className={classes.badge}>PHP</div>
+                  <div className={classes.badge}>JavaScript</div>
+                  <div className={classes.badge}>Mysql</div>
+                  <div className={classes.badge}>Ajax</div>
+                  <div className={classes.badge}>Jquery</div>
+                  <div className={classes.badge}>Android</div>
                 </div>
-
-             
-                
-               
-               
-              
-                {/* <div>
-                 
-                  <span className={classes_a.sub_span}>기간</span>2022-03-01 ~ 2022-06-01</div>
-                <div><span className={classes_a.sub_span}>담당 역할</span>web, app 유지보수 및 배포 경험 (50%)​</div>
-                <div><span className={classes_a.sub_span}>기술 스택 </span>PHP , JavaScript , Mysql, Ajax, Jquery,Android​</div>
-                <div><span className={classes_a.sub_span}>업무성과</span>
+                <div className={classes.title}>업무성과</div>
                   <p>1. Firebase 클라우드 메시징 푸시 기능​ 개선 ​</p>
                   <p>2. 토큰 생성 API 업데이트</p>
-                  <p>3. 레거시 코드 리팩토링(login, webview)</p>
+                  <p>3. 레거시 코드 리팩토링(login, webview)</p> 
+                
+                  <p><span>Web </span>  
+                    <button onClick={()=> {window.open(nrems_web)}}  className={classes.visit_small}>Web 방문하기</button>
+                  </p>
+                  <p><span>App </span>
+                    <button onClick={()=> {window.open(nrems_app)}}  className={classes.visit_small}>App Store 방문하기</button>
+                  </p>
+
+                
+                 
                 </div>
-                <div><span className={classes_a.sub_span}>web : </span>http://www.nrems.co.kr/login.php</div>
-                <div><span className={classes_a.sub_span}>app:</span>https://play.google.com/store/apps/details?id=kr.co.nrems&pcampaignid=web_share</div> */}
-        
-            </div>
+              </div>
           </div>
+        </div>
       </section>
     )
 
--- src/content/Project.js
+++ src/content/Project.js
@@ -7,7 +7,8 @@ import Portfolio from '../images/screen_pf.png'; // with import
 
 const Project = forwardRef(function Project(props, ref) {
 
-  const url = "https://bang04.github.io/";
+  const reactPortfolio = "https://portfolio-bek.vercel.app/";
+  const gatsbyblog = "https://bang04.github.io/";
 
     return(
       <section className={classes.section} ref={ref}>
@@ -24,23 +25,27 @@ const Project = forwardRef(function Project(props, ref) {
                 </figure>
               </div>
 
-              <div className={classes.box}>
+              <div className={classes.box_content}>
+                <button onClick={()=> {window.open(gatsbyblog)}} className={classes.visit_small}>Gatsby 블로그 방문하기</button>
                 <div className={classes.title}>Gatsby blog</div>
                 <div className={classes.desciption}>마크다운 파일을 활용한 gatsby 프레임워크 블로그</div>
                 <div className={classes.stack}>
                   <div className={classes.title}>기능</div>
-                  <p><span>검색</span>: 검색어가 제목과 내용에 포함된 경우 조회</p>
-                  <p><span>카테고리</span>: 카테고리별 목록</p>
-                  <p><span>태그</span> : 해당되는 태그 포함된 게시글 목록</p>
-                  <p><span>반응형웹 </span> : 웹과 모바일 최적화 </p>
-                  <div className={classes.title}>기술</div>
-                <p>html5</p>
-                <p>css3</p>
-                <p>react</p>
+                  <p><span>1.검색</span>: 검색어가 제목과 내용에 포함된 경우 조회</p>
+                  <p><span>2.카테고리</span>: 카테고리별 목록</p>
+                  <p><span>3.태그</span> : 해당되는 태그 포함된 게시글 목록</p>
+                  <p><span>4.반응형웹 </span> : 웹과 모바일 최적화 </p>
+      
+                  <div className={classes.title}>기술스택</div>
+                  <div className={classes.badges}>
+                    <div className={classes.badge}>Html</div>
+                    <div className={classes.badge}>Css3</div>
+                    <div className={classes.badge}>React</div>
+                  </div>
                   {/* 반응형 웹 
                   라이트/다크 모드
                   이메일 전송 */}
-                <button onClick={()=> {window.open(url)}} >GatsbyBlog 방문</button>
+               
               </div>
             </div>
           </div>
@@ -50,22 +55,25 @@ const Project = forwardRef(function Project(props, ref) {
                   <img src={Portfolio}  className={classes.thumbnail}/>
                 </figure>
               </div>
-              <div className={classes.box}>
+              <div className={classes.box_content}>
+                <button onClick={()=> {window.open(reactPortfolio)}}  className={classes.visit_small}>React Protfolio 방문하기</button>
                 <div className={classes.title}>React Portfolio</div>
                 <div className={classes.desciption}>리액트 기반의 포트폴리오</div>
                 <div className={classes.stack}>
                 
                 
                   <div className={classes.title}>기능</div>
-                  <p><span>검색</span>: 검색어가 제목과 내용에 포함된 경우 조회</p>
-                  <p><span>카테고리</span>: 카테고리별 목록</p>
-                  <p><span>태그</span> : 해당되는 태그 포함된 게시글 목록</p>
-                  <p><span>반응형웹 </span> : 웹과 모바일 최적화 </p>
-                  <div className={classes.title}>기술</div>
-                    <p>html5</p>
-                    <p>css3</p>
-                    <p>react</p>
-                  <button onClick={()=> {window.open(url)}} >GatsbyBlog 방문</button>
+                  <p><span>1.메뉴 슬라이딩</span> : useRef() 이용한 스크롤 이동</p>
+                  <p><span>2.메인 타이핑 효과</span> : Typewriter 사용</p>
+                  <p><span>3.반응형웹 </span> : 웹과 모바일 최적화 </p>
+          
+                  <div className={classes.title}>기술스택</div>
+                  <div className={classes.badges}>
+                    <div className={classes.badge}>Html</div>
+                    <div className={classes.badge}>Css3</div>
+                    <div className={classes.badge}>React</div>
+                  </div>
+                  
                 
               </div>
             </div>
